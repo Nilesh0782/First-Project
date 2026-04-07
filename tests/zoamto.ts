06e326bf2c604cf94876d0f159dd8633e2121eed
Nilesh pal <Nilesh.pal@newvision-software.com>
2026-04-07 10:53:24 +0530

    add zomato

---
+++ tests/zoamto.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test.use({ ignoreHTTPSErrors: true });
+
+test('has title', async ({ page }) => {
+  await page.goto('https://www.zomato.com/');
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://www.zomato.com/');
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
