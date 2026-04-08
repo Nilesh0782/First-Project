a130905c6146eef216c02bfd90ad2c8e101b7f49
Nilesh0782 <nilesh.pal@newvision-software.com>
2026-04-08 17:03:20 +0530

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -11,4 +11,6 @@ test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await page.getByRole('link', { name: 'Get started' }).click();
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+git changes
+  
 });
